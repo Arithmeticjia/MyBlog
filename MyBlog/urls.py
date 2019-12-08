8f15047ea0d2b09bc1afbcc9f5e48d178162dcaf
ArithmeticJia <1097197237@qq.com>
2019-12-08 22:59:17 +0800

    addJiaBlog

--- MyBlog/urls.py
+++ MyBlog/urls.py
@@ -14,8 +14,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import include
+from django.conf.urls import url
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from JiaBlog import views
+import notifications.urls
+from channels.routing import ProtocolTypeRouter
+
+# from django.views import static
+
+application = ProtocolTypeRouter({
+    # Empty for now (http->django views is added by default)
+})
 
 urlpatterns = [
+    url(r'^$', views.blog_index),
+    url(r'^baidu_verify_iYpMqoGJf4.html/$', views.baiduyz),
     path('admin/', admin.site.urls),
-]
+    path('JiaBlog/', include('JiaBlog.urls', namespace="JiaBlog")),
+    path('mdeditor/', include('mdeditor.urls')),
+    url(r'', include('social_django.urls', namespace='social')),
+    # url(r'^oauth/', include('social_django.urls', namespace='social')),
+    # url(r'^search/', include('haystack.urls')),                   # old way
+    url(r'^search/', views.MySeachView(), name='haystack_search'),  # new way
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}, name='static'),
+    # url(r'^media/(?P<path>.*)$',  serve, {"document_root": MEDIA_ROOT}),
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
