1177197d7b9b3a7954713d6b0185154795d0c6e6
ArithmeticJia <1097197237@qq.com>
2020-02-06 12:44:02 +0800

    fix bugs

--- JiaBlog/forms.py
+++ JiaBlog/forms.py
@@ -12,9 +12,11 @@ class CommentForm(forms.ModelForm):
         #fields = ['name','email','text']
         exclude = ['name','email','post','parentcomment']
 
+
 class UserForm(forms.Form):
     username = forms.CharField(label="用户名", max_length=128)
     password = forms.CharField(label="密码", max_length=256)
+
 
 class RegisterForm(forms.Form):
     gender = (
@@ -25,7 +27,7 @@ class RegisterForm(forms.Form):
     password1 = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     password2 = forms.CharField(label="确认密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(label="邮箱地址", widget=forms.EmailInput(attrs={'class': 'form-control'}))
-#sex = forms.ChoiceField(label='性别', choices=gender)
+    # sex = forms.ChoiceField(label='性别', choices=gender)
 
 
 class MessageForm(forms.ModelForm):
