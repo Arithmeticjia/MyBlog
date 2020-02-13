d60fcd8b89e7bdc66a49a1171299bc04b96fff93
ArithmeticJia <1097197237@qq.com>
2020-02-13 13:43:28 +0800

    add rss

--- JiaBlog/search_indexes.py
+++ JiaBlog/search_indexes.py
@@ -2,6 +2,7 @@ from haystack import indexes
 # 修改此处，为你自己的model
 from JiaBlog.models import Articles
 
+
 # 修改此处，类名为模型类的名称+Index，比如模型类为GoodsInfo,则这里类名为GoodsInfoIndex
 class ArticlesIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
